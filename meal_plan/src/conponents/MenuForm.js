d9d120e5fd440080065b0c85c563951c1654690d
Chocotapi <shioriavino@gmail.com>
2023-11-13 16:23:11 -0500

    Updated the form

--- meal_plan/src/conponents/MenuForm.js
+++ meal_plan/src/conponents/MenuForm.js
@@ -1,16 +1,29 @@
 import { useState } from 'react';
 
 function MenuForm () {
-    const [newValue, setNewValue] = useState('');
+    const [newTitle, setNewTitle] = useState('');
+    const [amount, setAmount] = useState('');
+    const [newRecipe, setNewRecipe] = useState('');
 
     const handleNameChange = (event) => {
-        setNewValue(event.target.value);
+        setNewTitle(event.target.value);
+    }
+
+    const handleAmountChange = (event) => {
+        const vegiesAmount = parseInt(event.target.value);
+        setAmount(vegiesAmount);
+    }
+
+    const handleRecipeChange = (event) => {
+        setNewRecipe(event.target.value);
     }
 
     const handleSubmit = (event) => {
         event.preventDefault();
         //addMenu
-        setNewValue('');
+        setNewTitle('');
+        setAmount('');
+        setNewRecipe('');
     }
 
     return (
@@ -22,48 +35,64 @@ function MenuForm () {
                         <label className="label">Title</label>
                         <input 
                             className="inout is-expanded"
-                            value={newValue}
+                            value={newTitle}
                             onChange={handleNameChange} 
                         />
                     </div>
                 </div>
                 <div className="field-group">
                     <div className="field">
-                        <label className="label">Whole Grain</label>
-                        <input 
-                            className="inout is-expanded"
-                            value={newValue}
-                            onChange={handleNameChange} 
-                        />
+                        <label className="label">Eat with</label>
+                        <div class="control">
+                            <div class="select">
+                                <select>
+                                <option>Rice</option>
+                                <option>Bread</option>
+                                <option>Pasta</option>
+                                <option>All of them</option>
+                                </select>
+                            </div>
+                        </div> 
                     </div>
                 </div>
                 <div className="field-group">
                     <div className="field">
                         <label className="label">Protein</label>
-                        <input 
-                            className="inout is-expanded"
-                            value={newValue}
-                            onChange={handleNameChange} 
-                        />
+                        <div class="control">
+                            <div class="select">
+                                <select>
+                                <option>Chicken</option>
+                                <option>Pork</option>
+                                <option>Beaf</option>
+                                <option>Beans</option>
+                                <option>Other</option>
+                                </select>
+                            </div>
+                        </div> 
                     </div>
                 </div>
                 <div className="field-group">
                     <div className="field">
-                        <label className="label">Vegetables</label>
+                        <label className="label">Vegetables Amount</label>
                         <input 
                             className="inout is-expanded"
-                            value={newValue}
-                            onChange={handleNameChange} 
+                            value={amount || ''}
+                            onChange={handleAmountChange} 
+                            type="number"
                         />
+                        <div>% of your entire meal</div>
                     </div>
                 </div>
+
+                {/* fruis Amount  */}
+
                 <div className="field-group">
                     <div className="field">
-                        <label className="label">Fruits</label>
+                        <label className="label">Recipe URL or Notes</label>
                         <input 
                             className="inout is-expanded"
-                            value={newValue}
-                            onChange={handleNameChange} 
+                            value={newRecipe}
+                            onChange={handleRecipeChange} 
                         />
                     </div>
                 </div>
