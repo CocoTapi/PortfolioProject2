e4b30ea35beac11c1a3c85d8c84eff4ba11498d8
Chocotapi <shioriavino@gmail.com>
2023-11-28 16:41:44 -0500

    setup Route function

--- meal_plan/src/App.js
+++ meal_plan/src/App.js
@@ -1,11 +1,23 @@
 //import Home from './conponents/Home';
 import Link from "./conponents/Link";
+import Route from "./conponents/Route";
+import Header from "./conponents/Header";
+import Navbar from "./conponents/Navbar";
 
 function App() {
 
   return (
     <div>
-      <Link></Link>
+      <Link to="/header">Go to Header</Link>
+      <Link to="/navbar">Go to Navbar</Link>
+      <div>
+        <Route path="/header">
+          <Header />
+        </Route>
+        <Route path="/navbar">
+          <Navbar />
+        </Route>
+      </div>
     </div>
   );
 }
